6a058005fec94eb465fdb607479bc22af8709503
sarascript <correasg.sara@gmail.com>
2018-04-05 16:11:41 +0200

    Item Collection done

---
+++ Prueba1/MyCell2.swift
@@ -0,0 +1,13 @@
+//
+//  MyCell2.swift
+//  Prueba1
+//
+//  Created by SARA CORREAS GORDITO on 5/4/18.
+//  Copyright © 2018 SARA CORREAS GORDITO. All rights reserved.
+//
+
+import UIKit
+
+class MyCell2: UICollectionViewCell {
+    
+}
---
+++ Prueba1/VCItem2.swift
@@ -0,0 +1,35 @@
+//
+//  VCItem2.swift
+//  Prueba1
+//
+//  Created by SARA CORREAS GORDITO on 5/4/18.
+//  Copyright © 2018 SARA CORREAS GORDITO. All rights reserved.
+//
+
+import UIKit
+
+class VCItem2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
