cb698c9b3779f88f1dfec5fc4d61fd9c00230b9c
sarascript <correasg.sara@gmail.com>
2018-05-07 11:35:41 +0200

    Register in DataHolder

--- Prueba1/DataHolder.swift
+++ Prueba1/DataHolder.swift
@@ -70,9 +70,35 @@ class DataHolder: NSObject {
         
     }
     
+    func signin(email: String, pass: String, repass: String, delegate:DataHolderDelegate){
+        var blEnd:Bool = false
+        DataHolder.sharedInstance.myUser.sEmail = email
+        if pass ==  repass {
+            Auth.auth().createUser(withEmail: email, password: pass) { (user, error) in
+                if (user != nil) {
+                    print("Te registraste con user ID: " + (user?.uid)!)
+                    // Add a new document with a generated ID
+                    DataHolder.sharedInstance.firestoreDB?.collection("Users").document((user?.uid)!).setData(DataHolder.sharedInstance.myUser.getMap()) { err in
+                        if let err = err {
+                            print("Error adding document: \(err)")
+                        } else {
+                            print("Document added with ID: ")
+                        }
+                    }
+                    blEnd = true
+                    delegate.DHDregisterComplete!(blEnd: true)
+                    //self.performSegue(withIdentifier: "trSignin", sender: self)
+                } else {
+                    print (error!)
+                }
+            }
+        }
+    }
+    
 }
 
 @objc protocol DataHolderDelegate {
     @objc optional func DHDdownloadReposComplete(blEnd:Bool)
     @objc optional func DHDloginComplete(blEnd:Bool)
+    @objc optional func DHDregisterComplete(blEnd:Bool)
 }
--- Prueba1/VCSignin.swift
+++ Prueba1/VCSignin.swift
@@ -10,7 +10,7 @@ import UIKit
 import FirebaseAuth
 import FirebaseFirestore
 
-class VCSignin: UIViewController {
+class VCSignin: UIViewController, DataHolderDelegate {
 
     @IBOutlet var txtEmail:UITextField?
     @IBOutlet var txtPass:UITextField?
@@ -24,6 +24,12 @@ class VCSignin: UIViewController {
 
         // Do any additional setup after loading the view.
     }
+    
+    func DHDregisterComplete(blEnd:Bool) {
+        if blEnd {
+            self.performSegue(withIdentifier: "trSignin", sender: self)
+        }
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -31,25 +37,26 @@ class VCSignin: UIViewController {
     }
     
     @IBAction func signin(){
-        DataHolder.sharedInstance.myUser.sEmail = txtEmail?.text
-        if txtPass?.text ==  txtRepeatPass?.text{
-            Auth.auth().createUser(withEmail: (txtEmail?.text)!, password: (txtPass?.text)!) { (user, error) in
-                if (user != nil) {
-                    print("Te registraste con user ID: " + (user?.uid)!)
+        DataHolder.sharedInstance.signin(email: (txtEmail?.text)!, pass: (txtPass?.text)!, repass: (txtRepeatPass?.text)!, delegate: self)
+       // DataHolder.sharedInstance.myUser.sEmail = txtEmail?.text
+       // if txtPass?.text ==  txtRepeatPass?.text{
+           // Auth.auth().createUser(withEmail: (txtEmail?.text)!, password: (txtPass?.text)!) { (user, error) in
+               // if (user != nil) {
+                   // print("Te registraste con user ID: " + (user?.uid)!)
                     // Add a new document with a generated ID
-                    DataHolder.sharedInstance.firestoreDB?.collection("Users").document((user?.uid)!).setData(DataHolder.sharedInstance.myUser.getMap()) { err in
-                        if let err = err {
-                            print("Error adding document: \(err)")
-                        } else {
-                            print("Document added with ID: ")
-                        }
-                    }
-                    self.performSegue(withIdentifier: "trSignin", sender: self)
-                } else {
-                    print (error!)
-                }
-            }
-        }
+                   // DataHolder.sharedInstance.firestoreDB?.collection("Users").document((user?.uid)!).setData(DataHolder.sharedInstance.myUser.getMap()) { err in
+                       // if let err = err {
+                          //  print("Error adding document: \(err)")
+                        //} else {
+                        //    print("Document added with ID: ")
+                      //  }
+                    //}
+                  //  self.performSegue(withIdentifier: "trSignin", sender: self)
+                //} else {
+              //      print (error!)
+            //    }
+          //  }
+        //}
     }
     
     @IBAction func cancel(){
